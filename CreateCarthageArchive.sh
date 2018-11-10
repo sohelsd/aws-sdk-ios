a7a681490e5d6e40814d95efba305b85dcebff8e
Chunqiang Sun <sdechunq@amazom.com>
2018-11-10 08:47:14 -0800

    test circeci configure

--- CreateCarthageArchive.sh
+++ CreateCarthageArchive.sh
@@ -1,15 +1,20 @@
-# carthage archive | tee "carthageout.txt"
-frameworkfilename=$(carthage archive |  grep -o 'Created .*\.zip$'  | grep -o   '\S*\.zip$')
+carthage archive | tee "carthageout.txt"
+frameworkfilename=$(cat carthageout.txt |  grep -o 'Created .*\.zip$'  | grep -o   '\S*\.zip$')
 
 if [ -z  "$frameworkfilename" ];then
-	echo "Cannot find framework zip file from output of carthage archive"
-	exit 1
+echo "Cannot find framework zip file from output of carthage archive"
+exit 1
 fi
 
 if [ ! -f "$frameworkfilename" ]; then
-	echo "$frameworkfilename is not present"
-	exit 1
+echo "$frameworkfilename is not present"
+exit 1
 fi
 
-mv $frameworkfilename   aws-sdk-ios-carthage.framework.zip
-
+mv $frameworkfilename aws-sdk-ios-carthage.framework.zip
+if [ -f "aws-sdk-ios-carthage.framework.zip" ]; then
+echo "$frameworkfilename is  present"
+exit 0
+else
+    exit 1
+fi
